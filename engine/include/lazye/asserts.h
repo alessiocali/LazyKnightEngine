e461d038ee51dab67a7abdacd56e072c3a08f8fb
Alessio <alessiog.cali@hotmail.com>
2020-08-15 15:18:11 +0300

    Assert is no longer printing messages ALWAYS

--- engine/include/lazye/asserts.h
+++ engine/include/lazye/asserts.h
@@ -12,7 +12,11 @@ namespace lazye
     void Assert(ConditionType condition, StringType message, Args... args)
     {
 #ifndef NDEBUG
-        ((std::cerr << message) << ... << args) << std::endl;
+        if (!condition)
+        {
+            ((std::cerr << message) << ... << args) << std::endl;
+        }
+        
         assert(condition);
 #endif // NDEBUG
     }
