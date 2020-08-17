ca526d5dc1ffb95f5a07388b3769fa60ea75484e
Alessio <alessiog.cali@hotmail.com>
2020-08-17 23:59:23 +0300

    Fixed fetch-dependencies on unix

--- fetch-dependencies.py
+++ fetch-dependencies.py
@@ -11,9 +11,9 @@ def is_vcpkg_initialized():
         return os.path.isfile("vcpkg/vcpkg")
 
 if not is_vcpkg_initialized():
-    vcpkg_script = "bootstrap-vcpkg.{0}".format("bat" if is_on_windows else "sh")
-    print("Initializing vcpkg: vcpkg/{0}".format(vcpkg_script))
-    subprocess.check_call(vcpkg_script, cwd="vcpkg", shell=True)
+    vcpkg_script = "vcpkg\\bootstrap-vcpkg.bat" if is_on_windows else "./vcpkg/bootstrap-vcpkg.sh"
+    print("Initializing vcpkg: {0}".format(vcpkg_script))
+    subprocess.check_call(vcpkg_script)
 
 dependencies_root = { }
 
@@ -22,4 +22,5 @@ with open("dependencies.json") as deps_file:
 
 for dependency in dependencies_root["dependencies"]:
     print("Installing {0}...".format(dependency["name"]))
-    subprocess.call([ "vcpkg", "install", dependency["VCPKG-ID"] ], cwd="vcpkg", shell=True)
+    vcpkg_command = "vcpkg\\vcpkg" if is_on_windows else "./vcpkg/vcpkg"
+    subprocess.call([ vcpkg_command, "install", dependency["VCPKG-ID"] ])
