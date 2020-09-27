c2b7d10cb1ef13614e1731abfc82a504690064cd
Alessio <alessiog.cali@hotmail.com>
2020-09-27 15:29:24 +0300

    Improved fetch-dependencies script
    Removed all path nonsense for an OS-independent solution (extensions are still OS dependent though...)

--- fetch-dependencies.py
+++ fetch-dependencies.py
@@ -1,19 +1,20 @@
 import os.path
+import pathlib
 import subprocess
 import json
 
 is_on_windows = os.name == "nt"
 
+vcpkg_dir = pathlib.Path().cwd() / "vcpkg"
+vcpkg_exe = vcpkg_dir / ("vcpkg.exe" if is_on_windows else "vcpkg")
+vcpkg_bootstrap = vcpkg_dir / ("bootstrap-vcpkg.bat" if is_on_windows else "bootstrap-vcpkg.sh")
+
 def is_vcpkg_initialized():
-    if is_on_windows:
-        return os.path.isfile("vcpkg/vcpkg.exe")
-    else:
-        return os.path.isfile("vcpkg/vcpkg")
+    return vcpkg_exe.is_file()
 
 if not is_vcpkg_initialized():
-    vcpkg_script = "vcpkg\\bootstrap-vcpkg.bat" if is_on_windows else "./vcpkg/bootstrap-vcpkg.sh"
-    print("Initializing vcpkg: {0}".format(vcpkg_script))
-    subprocess.check_call(vcpkg_script)
+    print("Initializing vcpkg")
+    subprocess.check_call(str(vcpkg_bootstrap))
 
 dependencies_root = { }
 
@@ -22,5 +23,4 @@ with open("dependencies.json") as deps_file:
 
 for dependency in dependencies_root["dependencies"]:
     print("Installing {0}...".format(dependency["name"]))
-    vcpkg_command = "vcpkg\\vcpkg" if is_on_windows else "./vcpkg/vcpkg"
-    subprocess.call([ vcpkg_command, "install", dependency["VCPKG-ID"] ])
+    subprocess.call([ str(vcpkg_exe), "install", dependency["VCPKG-ID"] ])
